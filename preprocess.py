d1d37efb1872fbffa13ca8a44867f947bb4a4b8d
David Lu <d4v1d_lu@hotmail.com>
2019-11-07 09:44:43 -0800

    removed unused functions

--- preprocess.py
+++ preprocess.py
@@ -10,65 +10,8 @@ import matplotlib.pyplot as plt
 propertyDF = pd.read_csv("property-tax-report.csv", sep=";")
 addressDF = pd.read_csv("property-addresses.csv", sep=";")
 censusDF = pd.read_csv("CensusLocalAreaProfiles2016.csv", encoding="ISO-8859-1")
-subset = propertyDF.head(250000)
+subset = propertyDF.head(200000)
 addr_subset = addressDF.head(100000)
-
-
-def changeCardinal():
-    count = 0
-    for row in addr_subset.itertuples():
-        addr = str(row.STD_STREET)
-        if addr[0:2] == "N " or addr[0:2] == "E " or addr[0:2] == "S " or addr[0:2] == "W ":
-            cardinal = addr[0]
-            addr_subset.at[count, 'STD_STREET'] = ''.join(addr[2:] + " " + cardinal)
-        count = count + 1
-    return
-
-
-def changeDrive():
-    count = 0
-    for row in addr_subset.itertuples():
-        addr = str(row.STD_STREET)
-        if re.search("\s{1}(Drive)", addr):
-            cardinal = addr[0]
-            addr_subset.at[count, 'STD_STREET'] = ''.join(addr[2:] + " " + cardinal)
-        count = count + 1
-    return
-
-
-def changeCivic():
-    count = 0
-    for row in subset.itertuples():
-        civic_code_to = (row.TO_CIVIC_NUMBER)
-        civic_code_from = (row.FROM_CIVIC_NUMBER)
-
-        if str(civic_code_to).isnumeric() and str(civic_code_from).isnumeric():
-            print("Civic To: ", civic_code_to)
-            print("Civic From: ", civic_code_from)
-            civic_code_to = float(civic_code_to)
-            civic_code_from = float(civic_code_from)
-
-            if (math.isnan(civic_code_to)):
-                print("went in one", civic_code_from)
-                civic_code_to = civic_code_from
-                subset.at[count, 'TO_CIVIC_NUMBER'] = civic_code_to
-
-            elif (math.isnan(civic_code_from)):
-                print("Went in two", civic_code_to)
-                civic_code_from = civic_code_to
-                subset.at[count, 'FROM_CIVIC_NUMBER'] = civic_code_from
-
-            elif civic_code_from < civic_code_to:
-                print("went in three")
-                tmp = civic_code_from
-                civic_code_from = civic_code_to
-                civic_code_to = tmp
-                print("CIVIC CODE: ", civic_code_from, civic_code_to)
-                subset.at[count, 'TO_CIVIC_NUMBER'] = civic_code_to
-                subset.at[count, 'FROM_CIVIC_NUMBER'] = civic_code_from
-        count = count + 1
-    return
-
 
 def addCensus(data):
     mandarinColumn = []
@@ -125,5 +68,6 @@ newdf = ps.sqldf(sqlcode, locals())
 
 addCensus(newdf)
 filterByYear(newdf, 2015)
+
 print(newdf.to_string())
 print(newdf.shape)
